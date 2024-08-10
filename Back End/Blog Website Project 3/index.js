e4e5fdeabfdc5d996ac7c7af4e804c339efb2a87
AnasSM0 <anasbutt20067@gmail.com>
2024-08-10 22:51:40 +0500

    second commit

--- Back End/Blog Website Project 3/index.js
+++ Back End/Blog Website Project 3/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000; 
 
+let blogs = [];
+  
+  module.exports = blogs;
+
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
