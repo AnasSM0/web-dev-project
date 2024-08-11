34b2e76c868a80a70217fbc9b041ae41da44902e
AnasSM0 <anasbutt20067@gmail.com>
2024-08-12 00:22:16 +0500

    3rd commit

--- Back End/Blog Website Project 3/index.js
+++ Back End/Blog Website Project 3/index.js
@@ -1,22 +1,43 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
-const port = 3000; 
-
-let blogs = [];
-  
-  module.exports = blogs;
+const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
+let blogList = [];
 
-res.render("index.ejs");
-
+app.get("/", (req, res) => {
+    res.render("index.ejs");
 });
 
-app.listen(port,()=>{
+app.get("/blogs", (req, res) => {
+    res.render("blogs.ejs", { blogs: blogList, message: null });
+});
 
-console.log(`Listening on port ${port}`);
+app.get("/create", (req, res) => {
+    res.render("newpost.ejs");
+});
 
+app.post("/create", (req, res) => {
+    const newBlog = {
+        email: req.body.email,
+        author: req.body.author,
+        category: req.body.category,
+        content: req.body.content,
+    };
+    blogList.push(newBlog);
+    res.redirect(`/blogs?message=Your blog post has been successfully submitted!`);
+});
+
+app.get("/blogs", (req, res) => {
+    const message = req.query.message;
+    res.render("blogs.ejs", { blogs: blogList, message: message });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
