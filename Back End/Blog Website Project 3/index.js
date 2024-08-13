9a8864c821bb512bc6f3ee8bac69949991066a1f
AnasSM0 <anasbutt20067@gmail.com>
2024-08-13 15:44:42 +0500

    home page, header, footer and other design and functional tweaks.

--- Back End/Blog Website Project 3/index.js
+++ Back End/Blog Website Project 3/index.js
@@ -33,9 +33,81 @@ app.post("/create", (req, res) => {
     res.redirect(`/blogs?message=Your blog post has been successfully submitted!`);
 });
 
-app.get("/blogs", (req, res) => {
-    const message = req.query.message;
-    res.render("blogs.ejs", { blogs: blogList, message: message });
+// Edit Blog
+app.get("/edit/:id", (req, res) => {
+    const blog = blogList[req.params.id];
+    res.render("editpost.ejs", { blog: blog, id: req.params.id });
+});
+
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    blogList[id] = {
+        email: req.body.email,
+        author: req.body.author,
+        category: req.body.category,
+        content: req.body.content,
+    };
+    res.redirect(`/blogs?message=Your blog post has been successfully updated!`);
+});
+
+// Delete Blog
+app.post("/delete/:id", (req, res) => {
+    blogList.splice(req.params.id, 1);
+    res.redirect(`/blogs?message=Your blog post has been successfully deleted!`);
+});
+
+// Full Blog Post Route
+app.get("/blogs/:id", (req, res) => {
+    const blog = blogList[req.params.id];
+    res.render("fullpost.ejs", { blog: blog });
+});
+
+app.get("/FAQ",(req,res) => {
+
+res.render("FAQ.ejs");
+
+});
+
+app.get("/about",(req,res)=>{
+
+res.render("about.ejs");
+
+})
+
+app.get("/contact",(req,res)=>{
+
+    res.render("contact.ejs");
+
+})
+
+app.get("/ai-automation",(req,res)=>{
+
+    res.render("partials/blog1.ejs");
+
+});
+
+app.get("/ux-ui-evolution",(req,res)=>{
+
+    res.render("partials/blog2.ejs");
+
+});
+
+app.get("/tech-insights",(req,res)=>{
+
+    res.render("partials/blog3.ejs");
+
+});
+
+app.get("/design-strategies",(req,res)=>{
+
+    res.render("partials/blog4.ejs");
+
+});
+
+app.get("/trends",(req,res)=>{
+
+    res.render("partials/blog5.ejs");
+
 });
 
 app.listen(port, () => {
