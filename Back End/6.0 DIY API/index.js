6d582a3d8859c00e544c68cd18e090f139f3d6dd
AnasSM0 <anasbutt20067@gmail.com>
2024-08-22 12:04:41 +0500

    fixed joke API CRUD requests

--- Back End/6.0 DIY API/index.js
+++ Back End/6.0 DIY API/index.js
@@ -9,19 +9,113 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
 
+app.get("/random",(req,res)=>{
 
+const randomIndex = Math.floor(Math.random * jokes.length);
+res.json(jokes[randomIndex])
+
+});
 
 //2. GET a specific joke
 
+app.get("/jokes/:id",(req,res)=>{
+
+const id = parseInt(req.params.id);
+const foundJoke = jokes.find((joke) => joke.id === id);
+res.json(foundJoke);
+
+});
+
 //3. GET a jokes by filtering on the joke type
+
+app.get("/filter",(req,res)=>{
+
+const type = req.query.type;
+const filtered = jokes.filter((joke)=>joke.jokeType === type);
+res.json(filtered);
+
+});
+
 
 //4. POST a new joke
 
+app.post("/jokes",(req,res)=>{
+
+const newJoke = {
+
+id: jokes.length + 1,
+jokeText: req.body.text,
+jokeType: req.body.type,
+
+};
+
+jokes.push(newJoke);
+console.log(jokes.slice(-1));
+res.json(newJoke);
+
+})
+
 //5. PUT a joke
+
+app.put("/jokes/:id",(req,res)=>{
+
+const id = parseInt(req.params.id);
+const replacementJoke = {
+
+id: id,
+jokeText: req.body.text,
+jokeType: req.body.type,
+
+};
+
+const searchIndex = jokes.findIndex((joke)=>joke.id === id);
+jokes[searchIndex] = replacementJoke;
+res.json(replacementJoke);
+
+});
 
 //6. PATCH a joke
 
+app.patch("/jokes/:id",(req,res)=>{
+
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke)=> joke.id === id);
+  
+  const replacementJoke = {
+  
+  id: id,
+  jokeText: req.body.text || existingJoke.jokeText,
+  jokeType: req.body.type || existingJoke.jokeType,
+  
+  };
+  
+  const searchIndex = jokes.findIndex((joke)=>joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+
+  });
+
+
 //7. DELETE Specific joke
+
+app.delete("/jokes/:id",(req,res)=>{
+
+const id = parseInt(req.params.id);
+const searchIndex = jokes.findIndex((joke)=>jokes.id===id);
+if (searchIndex>-1){
+
+  jokes.splice(searchIndex,1);
+  res.sendStatus(200);
+}
+
+else{
+
+  res.status(404).json({error: `Joke with id ${id} not found.
+    No Jokes were deleted.`});
+}
+
+});
+
 
 //8. DELETE All jokes
 
