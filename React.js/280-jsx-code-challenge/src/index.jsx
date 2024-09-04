8bbe9f5ea362cf553e08f0e8f36089d742511e32
AnasSM0 <anasbutt20067@gmail.com>
2024-09-04 15:52:09 +0500

    HTML elements using DOM

--- React.js/280-jsx-code-challenge/src/index.jsx
+++ React.js/280-jsx-code-challenge/src/index.jsx
@@ -6,7 +6,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-React.render()
+ReactDOM.render(
+
+<div>
+
+<h1>HELLO WORLD</h1>
+<ul>
+    <li>List item 1</li>
+    <li>List item 2</li>
+    <li>List item 3</li>
+</ul>
+</div>, document.getElementById("root")
+);
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
