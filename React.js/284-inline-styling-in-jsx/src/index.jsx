3c84dab78a52238da030eed2155a72f4987d7046
AnasSM0 <anasbutt20067@gmail.com>
2024-09-04 16:46:49 +0500

    inline styling with jsx

--- React.js/284-inline-styling-in-jsx/src/index.jsx
+++ React.js/284-inline-styling-in-jsx/src/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+
+color: "red",
+fontSize: "20px",
+border: "1px solid black",
+
+};
+
+ReactDOM.render(<h1 style = {customStyle}>Hello World!</h1>, document.getElementById("root"));
 
 // If you're running this locally in VS Code use the commands:
 // npm install
-// to install the node modules and
+// to install the node modules 
+
+
 // npm run dev
 // to launch your react project in your browser
