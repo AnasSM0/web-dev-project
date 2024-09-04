68bb0eb79d1b23a4eb50586a438cc936eb882be2
AnasSM0 <anasbutt20067@gmail.com>
2024-09-04 15:42:40 +0500

    first commit

---
+++ React.js/280-jsx-code-challenge/src/index.jsx
@@ -0,0 +1,14 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+React.render()
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
