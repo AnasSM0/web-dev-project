5864effdc1e72ff29bf0f160148d5d9b022e9a7a
AnasSM0 <anasbutt20067@gmail.com>
2024-09-05 15:53:33 +0500

    keeper app layout

---
+++ React.js/293-keeper-app-part-1-challenge/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+
+return ( <div  className="note">
+
+<h1 className="note">Title</h1>
+<p className="note">content</p>
+
+</div>
+);
+}
+
+export default Note;
