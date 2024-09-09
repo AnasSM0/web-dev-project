e9cbaeb5ca6278c12a1d5b7cc1bf6c7a8d4d837f
AnasSM0 <anasbutt20067@gmail.com>
2024-09-09 17:05:32 +0500

    first commit

---
+++ React.js/308-es6-destructuring/src/index.js
@@ -0,0 +1,30 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+import animals from "./data.js"
+
+
+const [cat,dog] = animals;
+
+console.log(cat);
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
