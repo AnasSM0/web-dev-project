85dc4b233ed8a3425b46b6211cf438de84c8dc55
AnasSM0 <anasbutt20067@gmail.com>
2024-09-13 11:46:07 +0500

    first commit

--- React.js/313-changing-complex-state/src/components/App.jsx
+++ React.js/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFname] = useState("");
+  const [lName, setLname] = useState("");
+
+  function UpdateFname(event) {
+    const firstName = event.target.value;
+    setFname(firstName);
+  }
+
+  function UpdateLname(event) {
+    const LastName = event.target.value;
+    setLname(LastName);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello{fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={UpdateFname}
+          name="fName"
+          placeholder="First Name"
+          value={fName}
+        />
+        <input
+          onChange={UpdateLname}
+          name="lName"
+          placeholder="Last Name"
+          value={lName}
+        />
         <button>Submit</button>
       </form>
     </div>
