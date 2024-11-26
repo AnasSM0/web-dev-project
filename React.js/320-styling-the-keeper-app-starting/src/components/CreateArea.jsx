f18048f5e74c6a2b230750b74de4f4828d4d49bf
AnasSM0 <anasbutt20067@gmail.com>
2024-11-26 15:37:36 +0500

    updated icons and styling

--- React.js/320-styling-the-keeper-app-starting/src/components/CreateArea.jsx
+++ React.js/320-styling-the-keeper-app-starting/src/components/CreateArea.jsx
@@ -4,6 +4,9 @@ import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+
+  const [isExpanded,setExpanded]=useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: "",
@@ -29,23 +32,30 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand(){
+    setExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
+        {isExpanded && (
         <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
+      )}
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          onClick={expand}
+          rows={isExpanded ? 3 : 1}
         />
-        <Zoom in={true}>
+        <Zoom in={isExpanded}>
           <Fab onClick={submitNote}>
             <AddIcon />
           </Fab>
